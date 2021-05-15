8c4c3579c7399b4bd498351a39250a6a7a856c9f
withWws <66957178+withWws@users.noreply.github.com>
2021-05-16 01:22:51 +0900

    pug, conditional state (if.. )

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,19 @@
+const fakeUser = {
+  username: 'with',
+  loggedIn: false,
+};
+
 // home controller
 export const trending = (req, res) =>
   res.render('home', {
     pageTitle: 'Home',
+    fakeUser: fakeUser,
   });
 
 // watch controller
 export const see = (req, res) =>
   res.render('watch', {
-    pageTitle: 'Watch',
+    pageTitle: 'Watch', // pug에 변수를 보낼 수 있다.
   });
 
 export const search = (req, res) => res.send('search');
--- src/server.js
+++ src/server.js
@@ -7,15 +7,24 @@ import logger from 'morgan';
 const PORT = 4000;
 const app = express();
 
-app.set('view engine', 'pug'); // pug 설정 (template engine == flask jinja)
+// ---- view engine 설정 ---->
+app.set('view engine', 'pug'); // view engine을 pug로 설정한다.
 app.set('views', process.cwd() + '/src/views'); // views fold의 default cwd를 수정한다.
-app.use(logger('dev')); //모든 경로에 logger 미들웨어를 실행시킴
+// ----
 
+// ---- middle ware 설정
+app.use(logger('dev')); //모든 경로에 logger 미들웨어를 실행시킴
+// ----
+
+// ---- router 설정 ---->
 app.use('/', globalRouter); // globalRouter 를 '/'의 하위로 그룹화 시킴
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
+// ----
 
+// ---- app.listen handler (app 실행했을 때 가장 처음에 실행되는 것)
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
+// ----
 
 app.listen(PORT, handleListening);
