a0f70bc2b5f12f2e63432c014b888330adebe57b
withWws <66957178+withWws@users.noreply.github.com>
2021-05-16 02:00:38 +0900

    mixin setting complete, ~ # 17.10

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,11 +4,36 @@ const fakeUser = {
 };
 
 // home controller
-export const trending = (req, res) =>
-  res.render('home', {
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'video #1',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 hours ago',
+      views: 100,
+    },
+    {
+      title: 'video #2',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 hours ago',
+      views: 100,
+    },
+    {
+      title: 'video #3',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 hours ago',
+      views: 100,
+    },
+  ];
+
+  return res.render('home', {
     pageTitle: 'Home',
-    fakeUser: fakeUser,
+    videos,
   });
+};
 
 // watch controller
 export const see = (req, res) =>
